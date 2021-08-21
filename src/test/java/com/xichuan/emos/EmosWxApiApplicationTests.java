c2ec2752e51ec9744193d2c61220b7bbb18ea8ac
xichuanwushao <avatarwx@163.com>
2021-08-21 16:51:45 +0800

    创建springboot空白项目

---
+++ src/test/java/com/xichuan/emos/EmosWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xichuan.emos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
