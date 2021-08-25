f6aa4d3ddb3759150a7a197f6e2f77f42f57164e
xichuanwushao <avatarwx@163.com>
2021-08-25 12:27:54 +0800

    3-9 创建OAuth2Filter类（二） 1.AuthenticationToken把token封装成认证对象 2.AuthenticationRealm定义认证与授权的实现方法 2.AuthenticationFilter拦截http请求 验证token 4.把设置应用到shiro框架

---
+++ src/main/java/com/xichuan/emos/shiro/OAuth2Realm.java
@@ -0,0 +1,44 @@
+package com.xichuan.emos.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class OAuth2Realm extends AuthorizingRealm {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof OAuth2Token;
+    }
+
+    /**
+     * 授权(验证权限时调用)
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection collection) {
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        //TODO 查询用户权限列表
+        //TODO 把权限列表添加到info对象
+        return info;
+    }
+
+    /**
+     * 认证(验证登录时调用)
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        //TODO 从令牌获取userId 然后检测该账户是否被冻结
+        SimpleAuthenticationInfo info=new SimpleAuthenticationInfo();
+        return info;
+    }
+}
