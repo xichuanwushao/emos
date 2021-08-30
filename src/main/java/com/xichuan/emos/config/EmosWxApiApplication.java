3b34276ae869fe63d520b25325d3de8a18fcda2d
xichuanwushao <avatarwx@163.com>
2021-08-30 17:00:18 +0800

    6-7 把数据表中数据读取出来 封装到java对象里面 1.在入口类初始化常量 自定义SysConfig的持久方法 2.缓存系统常量数据

--- src/main/java/com/xichuan/emos/config/EmosWxApiApplication.java
+++ src/main/java/com/xichuan/emos/config/EmosWxApiApplication.java
@@ -1,18 +1,49 @@
 package com.xichuan.emos.config;
 
+import cn.hutool.core.util.StrUtil;
+import com.xichuan.emos.domain.SysConfig;
+import com.xichuan.emos.mapper.SysConfigMapperCust;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.lang.reflect.Field;
+import java.util.List;
+@Slf4j
 @MapperScan("com.xichuan.emos.mapper")
 @ComponentScan("com.xichuan")
 @EnableSwagger2
 @SpringBootApplication
 public class EmosWxApiApplication {
+    @Resource
+    private SysConfigMapperCust sysConfigMapperCust;
 
+    @Autowired
+    private SystemConstants constants;
     public static void main(String[] args) {
         SpringApplication.run(EmosWxApiApplication.class, args);
     }
 
+    @PostConstruct
+    public void init(){
+        List<SysConfig> list=sysConfigMapperCust.selectAllParam();
+        list.forEach(one->{
+            String key=one.getParamKey();
+            key= StrUtil.toCamelCase(key);
+            String value=one.getParamValue();
+            try{
+                Field field=constants.getClass().getDeclaredField(key);
+                field.set(constants,value);
+            }catch (Exception e){
+                log.error("执行异常",e);
+            }
+        });
+
+    }
 }
---
+++ src/main/java/com/xichuan/emos/mapper/SysConfigMapperCust.java
@@ -0,0 +1,9 @@
+package com.xichuan.emos.mapper;
+
+import com.xichuan.emos.domain.SysConfig;
+
+import java.util.List;
+
+public interface SysConfigMapperCust {
+    public List<SysConfig> selectAllParam();
+}
