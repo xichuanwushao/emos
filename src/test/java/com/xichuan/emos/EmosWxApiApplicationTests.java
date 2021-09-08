03cc822aee249fadc47b4b87ed553fb8179875f0
xichuanwushao <avatarwx@163.com>
2021-09-08 16:30:06 +0800

    9-14 插入500万条消息记录到mongoDb 用于app分页开发

--- src/test/java/com/xichuan/emos/EmosWxApiApplicationTests.java
+++ src/test/java/com/xichuan/emos/EmosWxApiApplicationTests.java
@@ -1,13 +1,43 @@
 package com.xichuan.emos;
 
+import cn.hutool.core.util.IdUtil;
+import com.xichuan.emos.config.EmosWxApiApplication;
+import com.xichuan.emos.domain.MessageEntity;
+import com.xichuan.emos.domain.MessageRefEntity;
+import com.xichuan.emos.service.MessageService;
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import javax.annotation.Resource;
+import java.util.Date;
+
+@SpringBootTest(classes = EmosWxApiApplication.class)
 class EmosWxApiApplicationTests {
 
+    @Resource
+    private MessageService messageService;
     @Test
     void contextLoads() {
+        for (int i = 1; i <= 5000000; i++) {
+            MessageEntity message = new MessageEntity();
+            message.setUuid(IdUtil.simpleUUID());
+            message.setSenderId(0);
+            message.setSenderName("系统消息");
+            message.setMsg("这是第" + i + "条测试消息");
+            message.setSendTime(new Date());
+            String id=messageService.insertMessage(message);
+
+            MessageRefEntity ref=new MessageRefEntity();
+            ref.setMessageId(id);
+            ref.setReceiverId(11); //接收人ID
+            ref.setLastFlag(true);
+            ref.setReadFlag(false);
+            messageService.insertRef(ref);
+        }
     }
+
+
 
 }
